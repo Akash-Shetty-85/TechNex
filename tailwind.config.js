aa6bcd1ab7cf314ca93521eed32cb0746dee1a58
Akash <mr.shetty28502@gmail.com>
2023-11-16 23:44:47 +0530

    tailwind configuration,color setting

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    // extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "#38cabc",
+      background:"#f4f2f3",
+      heading:'#3a3333',
+      paragraph:'#7a7a7a',
+    },
+  },
+  plugins: [],
+};
